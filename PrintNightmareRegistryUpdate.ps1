433d87d9096779c453a136f03da18f170c7893cb
JohnDumas57 <105819043+JohnDumas57@users.noreply.github.com>
2022-06-20 13:37:49 -0500

    Add files via upload

---
+++ PrintNightmareRegistryUpdate.ps1
@@ -0,0 +1,7 @@
+﻿$Path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint"
+If((Get-ItemPropertyValue -Path $Path -Name NoWarningNoElevationOnInstall -ErrorAction SilentlyContinue) -eq "1"){
+    Set-ItemProperty -Path $Path -Name NoWarningNoElevationOnInstall -Value 0
+}
+If((Get-ItemPropertyValue -Path $Path -Name UpdatePromptSettings -ErrorAction SilentlyContinue) -eq "1"){
+    Set-ItemProperty -Path $Path -Name UpdatePromptSettings -Value 0
+}
